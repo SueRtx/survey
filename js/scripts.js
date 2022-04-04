3fa1906e210492748a993f5f35d66e6cbb485204
Sue Roberts <suerobertstx@yahoo.com>
2022-04-04 13:55:47 -0500

    Add index.html, styles.css, scripts.js, bootstrap, and jquery.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
+
+const beverage = $("#beverage").val();
+
+const dob = $("#born").val();
+
+const favoriteColor = $("#color").val();
+});
+});
